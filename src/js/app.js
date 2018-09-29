0b26ee38b47a63390249785451431fdafe5bcb31
Carolina Lastra <carolastra.cofre@gmail.com>
2018-09-29 17:14:11 -0300

    nav-bar y diseño principal

---
+++ src/js/app.js
@@ -0,0 +1,35 @@
+AOS.init();
+
+
+// Animación texto
+$(document).ready(function() {
+  let mouseX, mouseY;
+  let ww = $(window).width();
+  let wh = $(window).height();
+  let traX, traY;
+  $(document).mousemove(function(event) {
+    mouseX = event.pageX;
+    mouseY = event.pageY;
+    traX = ((7 * mouseX) / 570) + 80;
+    traY = ((7 * mouseY) / 570) + 90;
+    console.log(traX);
+    $('.title').css({'background-position': traX + '%' + traY + '%' });
+  });
+});
+
+function aboutMe() {
+  location.href = '#aboutMe';
+}
+
+function navBarMenu() {
+  location.href = '#navBarMenu';
+}
+
+function projects() {
+  location.href = '#projects';
+}
+
+function skills() {
+  location.href = '#skills';
+}
+
